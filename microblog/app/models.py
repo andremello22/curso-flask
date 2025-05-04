62cadb32ee1c8e8d9c4681c3e2ca1f784c9d7ed3
unknown <andremelloti@outlook.com>
2025-05-04 16:38:04 -0300

    banco de dados incompleto

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa 
 import sqlalchemy.orm as so
@@ -9,7 +10,26 @@ class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     user_name: so.Mapped[str] = so.mapped_column(sa.String(64), unique=True, index=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), unique=True, index=True)
-    password_hash: so.Mapped[str] = so.mapped_column(sa.String(256))
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
 
     def __repr__(self):
         return f'<User {self.user_name}>'
+
+
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc)
+    )
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+   
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+
+    def __repr__(self):
+        return f'<Post {self.body}>'
